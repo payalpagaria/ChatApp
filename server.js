5b4c09f40ef5c0f9df20e524830a31bf2d775cde
payalpagaria <pagariapayal67@gmail.com>
2023-01-03 22:47:28 +0530

    Prompt user after joining

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const path=require('path')
 const express=require('express')
+const socketio=require('socket.io')
+const http=require('http')
 const app=express()
+const server=http.createServer(app);
+const io=socketio(server);
 app.use(express.static(path.join(__dirname,'public')))
+// run when client connects
+io.on('connection', (socket) => {
+    console.log('someone connected!');
+    socket.emit('message',' Welcome to the chat');
+    socket.broadcast.emit("hello", "world");
+
+});
+
 const PORT=3000 || process.env.PORT;
-app.listen(PORT,()=>{
+
+server.listen(PORT,()=>{
     console.log(`The server is running on port ${PORT}`);
 })
