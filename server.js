4f736f87797d9fe0293b4de0584ef8c67a86b70c
payalpagaria <pagariapayal67@gmail.com>
2023-01-03 21:29:56 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const path=require('path')
+const express=require('express')
+const app=express()
+app.use(express.static(path.join(__dirname,'public')))
+const PORT=3000 || process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`The server is running on port ${PORT}`);
+})
