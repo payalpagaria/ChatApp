b1186f0ce52346ed217abbcb59bfc0bbc2ad074a
payalpagaria <pagariapayal67@gmail.com>
2023-01-04 15:08:56 +0530

    emitted message fro client

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,15 @@
+const chatform=document.getElementById('chat-form');
 const socket=io()
 
 socket.on('message',(message)=>{
     console.log(message);
 })
 
+chatform.addEventListener('submit',(e)=>{
+    //when u submit a form it submits to a file we want prevent that
+    e.preventDefault();
+    //get msg text
+    const msg=e.target.elements.msg.value;
+    //emitting to server 00
+    socket.emit('chatMessage',msg);
+})
