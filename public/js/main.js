7333ec02e92445335d0aad55dec4f174edd66598
payalpagaria <pagariapayal67@gmail.com>
2023-01-04 15:22:54 +0530

    Added Dom to display msg on the frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
 const chatform=document.getElementById('chat-form');
 const socket=io()
-
+//Message from server
 socket.on('message',(message)=>{
     console.log(message);
+    outputMessage(message);
 })
 
 chatform.addEventListener('submit',(e)=>{
@@ -13,3 +14,14 @@ chatform.addEventListener('submit',(e)=>{
     //emitting to server 00
     socket.emit('chatMessage',msg);
 })
+//output msg to dom
+function outputMessage(message){
+    const div=document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML=`<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+
+}
