f7639cbaee65acc622a968596e69e14fae0e6428
payalpagaria <pagariapayal67@gmail.com>
2023-01-05 22:57:44 +0530

    updates files

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 const chatform=document.getElementById('chat-form');
 const chatMessages=document.querySelector('.chat-messages');
+//Get username and room from url
+const{username,room}=Qs.parse(location.search,{
+    ignoreQueryPrefix:true
+})
 const socket=io()
+//join chatroom
+socket.emit('joinRoom',{username,room});
 //Message from server
 socket.on('message',(message)=>{
     console.log(message);
@@ -9,6 +15,7 @@ socket.on('message',(message)=>{
     chatMessages.scrollTop=chatMessages.scrollHeight;
 
 })
+
 
 chatform.addEventListener('submit',(e)=>{
     //when u submit a form it submits to a file we want prevent that
@@ -23,13 +30,17 @@ chatform.addEventListener('submit',(e)=>{
     e.target.elements.msg.focus();
 })
 //output msg to dom
-function outputMessage(message){
-    const div=document.createElement('div');
+function outputMessage(message) {
+    const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML=`<p class="meta">${message.username} <span>${message.time}</span></p>
-    <p class="text">
-        ${message.text}
-    </p>`;
+    const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = message.username;
+    p.innerHTML += `<span>${message.time}</span>`;
+    div.appendChild(p);
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = message.text;
+    div.appendChild(para);
     document.querySelector('.chat-messages').appendChild(div);
-
-}
+  }
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express=require('express')
 const socketio=require('socket.io')
 const http=require('http')
 const formatMessage = require("./utils/messages");
-
+const {userJoin,getCurrentuser}=require("./utils/users");
 const app=express()
 const server=http.createServer(app);
 const io=socketio(server);
@@ -11,18 +11,23 @@ app.use(express.static(path.join(__dirname,'public')))
 // run when client connects
 const botname='Chat bot'
 io.on('connection', (socket) => {
-    console.log('someone connected!');
-    socket.emit('message',formatMessage(botname,' Welcome to the chat'));
-    //broadcast when a user connects
-    socket.broadcast.emit('message', formatMessage(botname,'A user has entered'));
+    socket.on('joinRoom',({username,room})=>{
+       const user=userJoin(socket.id,username,room);
+       socket.join(user.room);
+        socket.emit('message',formatMessage(botname,' Welcome to the chat'));
+        //broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botname,`${user.username} has entered`));
+    
+    })
     // Runs when client disconnects
-    socket.on('disconnect',()=>{
-        io.emit('message','user has left the chat')
-    });
+    
     //listen for chat msg came from frontend 00
     socket.on('chatMessage',(msg)=>{
         io.emit('message',msg);
     })
+    socket.on('disconnect',()=>{
+        io.emit('message','user has left the chat')
+    });
 });
 
 
